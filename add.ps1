8058e696b237e2e45c7fb681401e1c435b9c0594
Dayne Pacheco <daynepacheco@gmail.com>
2024-05-08 16:48:14 -0300

    fazendo o add ser mais especifico em qual arquivo add

--- add.ps1
+++ add.ps1
@@ -1,14 +1,27 @@
 function add {
+    param(
+        [string]$param
+    )
+
     if(-not(Test-Path .daan/ADD)){
         return
-    }
-    Set-Content -Path .daan/ADD -Value ""
+    }   
 
-    $list = (Get-ChildItem).Name
+    if ($param -eq "."){
+        $list = (Get-ChildItem).Name
 
-    foreach ($item in $list) {
-        if ($item -ne ".daan"){
-            Add-Content -Path .daan/ADD -Value $item
+        Set-Content -Path .daan/ADD -Value ""
+
+        foreach ($item in $list) {
+            if ($item -ne ".daan"){
+                Add-Content -Path .daan/ADD -Value $item
+            }
         }
+    }else{
+        if(-not(Test-Path $param)){
+            return
+        }  
+        Add-Content -Path .daan/ADD -Value $param
     }
+    
 }
